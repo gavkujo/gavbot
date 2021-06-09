900cdfe1b68d7e4637ad0a55bd7b4280eb5c5385
AdityaBhattacharya1 <69953590+AdityaBhattacharya1@users.noreply.github.com>
2021-06-09 14:37:13 +0530

    minor bug fixes and refactors

--- bot.py
+++ bot.py
@@ -2,119 +2,139 @@ import discord
 import os
 import re
 import random
-import time
 import sqlite3
+import asyncio
 
-con = sqlite3.connect('test.db')
+from dotenv import load_dotenv
+from os.path import dirname, join
+
+dotenv_path = join(dirname(__file__), ".env")
+load_dotenv(dotenv_path)
+DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
+
+
+con = sqlite3.connect("test.db")
 
 cur = con.cursor()
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f"We have logged in as {client.user}")
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
-      return
-    
+        return
+
     if message.content.startswith("-reg"):
-      baba = message.author.id
-      cur.execute("SELECT EXISTS(SELECT 1 FROM ramdev WHERE user_id=:hm LIMIT 1)", {"hm": baba})
+        baba = message.author.id
+        cur.execute("SELECT EXISTS(SELECT 1 FROM ramdev WHERE user_id=:hm LIMIT 1)", {"hm": baba})
 
-	  # why not initialise record as cur.fetchone()[0]?
-    # i copied this part from somewhere so i dont wanna risk it.
-	# K.
-      record = cur.fetchone()
+        record = cur.fetchone()
 
-	  # no need of doing == 1 here. just do if record[0]
-	  # look into truthy falsy values for python
-      if record[0] == 1:
-        await message.channel.send("dude you are already registered.")
-      else:
-        cur.execute("INSERT INTO ramdev VALUES (?, ?)", (baba, 1000))
-        print("new")
-        await message.channel.send("successfully registered")
-        con.commit()
-      
+        if record[0]:
+            await message.channel.send("dude you are already registered.")
+        else:
+            cur.execute("INSERT INTO ramdev VALUES (?, ?)", (baba, 1000))
+            print(f"New user registered. UserId: {baba}")
+            await message.channel.send("successfully registered")
+            con.commit()
+
     if message.content.startswith("-pky"):
-      baba = message.author.id
-      cur.execute("SELECT EXISTS(SELECT 1 FROM ramdev WHERE user_id=:hm LIMIT 1)", {"hm": baba})
-      record = cur.fetchone()
-      if record[0] == 1:
-        
-        moni = cur.execute("select moni from ramdev where user_id =:hm", {"hm": baba})
-        moni = cur.fetchone()
+        baba = message.author.id
+        cur.execute("SELECT EXISTS(SELECT 1 FROM ramdev WHERE user_id=:hm LIMIT 1)", {"hm": baba})
+        record = cur.fetchone()
+        if record[0]:
 
-        money = moni[0]
-        
-        await message.channel.send(f"{message.author.mention}, you have {money} pakistani yen(the greatest currency of all time allah hu akbar)")
-      else:
-        await message.channel.send("you are not registered for gambling in this karachi casino of hamood. to register, type '-reg'")
+            moni = cur.execute("select moni from ramdev where user_id =:hm", {"hm": baba})
+            moni = cur.fetchone()
 
-    if message.content.startswith('-gg'):
-      await message.channel.send('hm ok. lemme check')
-      baba = message.author.id
-      cur.execute("SELECT EXISTS(SELECT 1 FROM ramdev WHERE user_id=:hm LIMIT 1)", {"hm": baba})
-      record = cur.fetchone()
-      if record[0] == 1:
-        
-        moni = cur.execute("select moni from ramdev where user_id =:hm", {"hm": baba})
+            money = moni[0]
 
-        moni = cur.fetchone()[0]
-        
-        x = int(re.sub('[^0-9]', '', message.content))
-        if x > moni:
-          await message.channel.send('ur broke bruv, stay in ur awkat')
+            await message.channel.send(
+                f"{message.author.mention}, you have {money} pakistani yen(the greatest currency of all time allah hu akbar)"
+            )
         else:
-          cur.execute("update ramdev set moni = moni-:bet where user_id =:hm", {"hm": baba, "bet": x})
-          con.commit()
-          await message.channel.send(f'uh, {message.author.mention}, the spin wheel spins...')
-          time.sleep(10)
-          spin = random.randint(1,7)
-          if spin < 3:
-            await message.channel.send(f'hahaha {message.author.mention}, get fucked! you aint getting money')
-            print(spin)
-          if spin == 3:
-            await message.channel.send(f'{message.author.mention},damn you got the joker thing. check ur money.')
-            jokermoni = random.randint(100, 10000)
-            cur.execute("update ramdev set moni = moni+:bet where user_id =:hm", {"hm": baba, "bet": x+jokermoni})
-            con.commit()
-          if spin == 4:
-            await message.channel.send(f'uh {message.author.mention}, you got ur money back but you didnt get any bonus or shit like that')
-            cur.execute("update ramdev set moni = moni+:bet where user_id =:hm", {"hm": baba, "bet": x})
-            con.commit()
+            await message.channel.send(
+                "you are not registered for gambling in this karachi casino of hamood. to register, type '-reg'"
+            )
 
-          if  5 <= spin < 7:
-            await message.channel.send(f'{message.author.mention}, you got double the money you bet')
-            cur.execute("update ramdev set moni = moni+:bet where user_id =:hm", {"hm": baba, "bet": x*2})
-            con.commit()
-          if spin == 7:
-            await message.channel.send(f"{message.author.mention}, you got triple the money you bet")
-            cur.execute("update ramdev set moni = moni+:bet where user_id =:hm", {"hm": baba, "bet": x*3})
-            con.commit()
-      else:
-        await message.channel.send("you are not registered for gambling in this karachi casino of hamood. to register, type '-reg'")
+    if message.content.startswith("-gg"):
+        await message.channel.send("hm ok. lemme check")
+        baba = message.author.id
+        cur.execute("SELECT EXISTS(SELECT 1 FROM ramdev WHERE user_id=:hm LIMIT 1)", {"hm": baba})
+        record = cur.fetchone()
+        if record[0]:
+
+            moni = cur.execute("select moni from ramdev where user_id =:hm", {"hm": baba})
+
+            moni = cur.fetchone()[0]
+
+            x = int(re.sub("[^0-9]", "", message.content))
+            if x > moni:
+                await message.channel.send("ur broke bruv, stay in ur awkat")
+            else:
+                cur.execute("update ramdev set moni = moni-:bet where user_id =:hm", {"hm": baba, "bet": x})
+                con.commit()
+                await message.channel.send(f"uh, {message.author.mention}, the spin wheel spins...")
+                await asyncio.sleep(10)
+                spin = random.randint(1, 7)
+                if spin < 3:
+                    await message.channel.send(f"hahaha {message.author.mention}, get fucked! you aint getting money")
+                    print(f"Spin number: ${spin}")
+                if spin == 3:
+                    await message.channel.send(
+                        f"{message.author.mention},damn you got the joker thing. check ur money."
+                    )
+                    jokermoni = random.randint(100, 10000)
+                    cur.execute(
+                        "update ramdev set moni = moni+:bet where user_id =:hm", {"hm": baba, "bet": x + jokermoni}
+                    )
+                    con.commit()
+                if spin == 4:
+                    await message.channel.send(
+                        f"uh {message.author.mention}, you got ur money back but you didnt get any bonus or shit like that"
+                    )
+                    cur.execute("update ramdev set moni = moni+:bet where user_id =:hm", {"hm": baba, "bet": x})
+                    con.commit()
+
+                if 5 <= spin < 7:
+                    await message.channel.send(f"{message.author.mention}, you got double the money you bet")
+                    cur.execute("update ramdev set moni = moni+:bet where user_id =:hm", {"hm": baba, "bet": x * 2})
+                    con.commit()
+                if spin == 7:
+                    await message.channel.send(f"{message.author.mention}, you got triple the money you bet")
+                    cur.execute("update ramdev set moni = moni+:bet where user_id =:hm", {"hm": baba, "bet": x * 3})
+                    con.commit()
+        else:
+            await message.channel.send(
+                "you are not registered for gambling in this karachi casino of hamood. to register, type '-reg'"
+            )
     if message.content.startswith("-moni"):
-      baba = message.author.id
-      cur.execute("SELECT EXISTS(SELECT 1 FROM ramdev WHERE user_id=:hm LIMIT 1)", {"hm": baba})
-      record = cur.fetchone()
-      if record[0] == 1:
-        moni = cur.execute("select moni from ramdev where user_id =:hm", {"hm": baba})
+        baba = message.author.id
+        cur.execute("SELECT EXISTS(SELECT 1 FROM ramdev WHERE user_id=:hm LIMIT 1)", {"hm": baba})
+        record = cur.fetchone()
+        if record[0]:
+            moni = cur.execute("select moni from ramdev where user_id =:hm", {"hm": baba})
 
-        moni = cur.fetchone()[0]
-        
-        if moni > 2000:
-          await message.channel.send("dude you have enough money")
+            moni = cur.fetchone()[0]
 
+            if moni > 2000:
+                await message.channel.send("dude you have enough money")
+
+            else:
+                cur.execute("update ramdev set moni = 2000 where user_id =:hm", {"hm": baba})
+
+                con.commit()
+                await message.channel.send(f"{message.author.mention}, you have 2000 pky.")
         else:
-          cur.execute("update ramdev set moni = 2000 where user_id =:hm", {"hm": baba})
+            await message.channel.send(
+                "you are not registered for gambling in this karachi casino of hamood. to register, type '-reg'"
+            )
 
-          con.commit()
-          await message.channel.send(f"{message.author.mention}, you have 2000 pky.")
-      else:
-        await message.channel.send("you are not registered for gambling in this karachi casino of hamood. to register, type '-reg'")
 
-client.run(os.getenv('token'))
+client.run(DISCORD_TOKEN)
